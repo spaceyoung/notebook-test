23f6344e95e968b27419bedb2155231d4a30a415
minyoung <myoung.ever@gmail.com>
2024-02-12 15:04:31 +0900

    change firebase sdk

--- src/datasources/firebase.js
+++ src/datasources/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, query, orderBy } from 'firebase/firestore';
 
 const notebookDB = initializeApp({
-  apiKey: import.meta.env.VITE_FB_APP_KEY,
-  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FB_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FB_APP_ID,
-  measurementId: import.meta.env.VITE_FB_MEASUREMENT_ID,
+  apiKey: "AIzaSyBWNEluo62wozmcwgNUp3GS3ahtvBbj8Dg",
+  authDomain: "notebook-56d13.firebaseapp.com",
+  projectId: "notebook-56d13",
+  storageBucket: "notebook-56d13.appspot.com",
+  messagingSenderId: "235500647824",
+  appId: "1:235500647824:web:c087ab481bbfc0cd816f15",
+  measurementId: "G-J7GPRYGWV3"
 });
 
 export const database = getFirestore(notebookDB);
